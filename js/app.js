f009ed9fc9b9e04c588ec1c116e8073fa24fb93d
seancov <rsecovington.subscriptions@gmail.com>
2016-12-02 14:18:32 -0600

    console.log events

--- js/app.js
+++ js/app.js
@@ -6,5 +6,14 @@ $(document).ready(function () {
     $('.ryu').mouseleave(function () {
         $('.ryu-ready').hide();
         $('.ryu-still').show();
-    });
+    })
+    $('.ryu').mousedown(function () {
+            console.log('mousedown');
+            // play hadouken sound
+            // show hadouken and animate it to the right of the screen
+        })
+        .mouseup(function () {
+            console.log('mouseup');
+            // ryu goes back to his ready position
+        });
 });
